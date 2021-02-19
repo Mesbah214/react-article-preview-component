59609927d05780ab2c1b925e222e8db295afde6d
Mesbah <msbh214@gmail.com>
2021-02-19 20:40:19 +0600

    added CardProps

---
+++ src/Resources/CardProps.js
@@ -0,0 +1,22 @@
+import card1Img from "./images/drawers.jpg";
+import card1AuthorImage from "./images/avatar-michelle.jpg";
+
+export const CardProps = [
+  {
+    id: 1,
+
+    image: card1Img,
+
+    title:
+      "Shift the overall look and feel by adding these wonderful touches to furniture in your home",
+
+    description:
+      "Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.",
+
+    author: {
+      image: card1AuthorImage,
+      name: "Michelle Appleton",
+      dateOfPublish: "28 Jun 2020",
+    },
+  },
+];
